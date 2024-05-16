29f3d297d687f07af10c706500cf7122a797aaeb
ankitsaini-github <ankit121saini@gmail.com>
2024-05-16 21:48:38 +0530

    Retrieving data from databases

--- util/database.js
+++ util/database.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  host: 'local',
-  user: 'root',
-  database: 'node-complete',
-  password: 'root123'
+    host: 'localhost',
+    user: 'root',
+    database: 'node-complete',
+    password: 'root123'
 });
 
 module.exports = pool.promise();
